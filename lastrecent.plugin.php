71aa11012a553e6d0d855c1f55f97c649250fa6c
lildude <USER@DOMAIN>
2011-09-06 17:03:45 +0000

    plugin:lastrecent Significantly updated and improved plugin.
    Changes include:
    - Added 0.7 tag
    - Improved help
    - Moved beacon to xml file
    - Added block support without removing "legacy" support
    - Coding standards
    
    This plugin could do with renaming to lastfm_recent or lastfmrecent as lastrecent isn't really clear.  Maybe another day.
    
    
    
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/lastrecent/trunk@3999 f4a90942-1786-4aa1-b092-57c31935ddc3

--- lastrecent.plugin.php
+++ lastrecent.plugin.php
@@ -1,127 +1,144 @@
 <?php
 class LastRecent extends Plugin
 {
-	private $api_key= 'd4a06492b226a89f358cf9dbd687001b';
-	private $cache_expiry= '7200';
-	private $uuid= 'eeaf6421-abca-4999-95d0-512d328d2462';
-	private $user= null;
-	private $limit= 3;
-	private $images= 1;
+	private $api_key = 'd4a06492b226a89f358cf9dbd687001b';
+	private $cache_expiry = '7200';
+	private $user = null;
+	private $limit = 3;
+	private $images = 1;
 
 	public function action_init()
 	{
 		$this->add_template( 'lastfm', dirname( __FILE__ ) . '/lastfm.php' );
-		$this->user= Options::get( 'lastrecent__user' );
-		$this->limit= Options::get( 'lastrecent__limit' );
-		$size= Options::get( 'lastrecent__images' );
+		$this->add_template( "block.lastfm_recent", dirname( __FILE__ ) . "/block.lastfm_recent.php" );
+		$this->user = Options::get( 'lastrecent__user' );
+		$this->limit = Options::get( 'lastrecent__limit' );
+		$size = Options::get( 'lastrecent__images' );
 		switch( $size ) {
-			case 'none': $this->images= null; break;
-			case 'small': $this->images= 0; break;
-			case 'medium': $this->images= 1; break;
-			case 'large': $this->images= 2; break;
+			case 'none': $this->images = null; break;
+			case 'small': $this->images = 0; break;
+			case 'medium': $this->images = 1; break;
+			case 'large': $this->images = 2; break;
+			case 'extralarge': $this->images = 3; break;
 		}
 	}
 
-	public function action_plugin_activation( $file )
+	public function action_plugin_activation()
 	{
-		if ( realpath( $file ) == __FILE__ ) {
-			// a cronjob to fetch the data from last.fm
-			CronTab::add_hourly_cron( 'lastrecent', 'lastrecent', 'Fetch recent tracks from last.fm' );
-		}
+		// a cronjob to fetch the data from last.fm
+		CronTab::add_hourly_cron( 'lastrecent', 'lastrecent', _t( 'Fetch recent tracks from last.fm', 'lastrecent' ) );
 	}
 
 	function action_plugin_deactivation( $file )
 	{
-		if ( Plugins::id_from_file($file) == Plugins::id_from_file(__FILE__) ) {
-			CronTab::delete_cronjob( 'lastrecent' );
-			Cache::expire( 'lastrecent' );
-		}
+		CronTab::delete_cronjob( 'lastrecent' );
+		Cache::expire_group( 'lastrecent' );
 	}
 
-	public function filter_plugin_config( $actions, $plugin_id )
+	public function filter_plugin_config( $actions )
 	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			$actions[]= _t('Configure');
-		}
+		$actions['configure']= _t( 'Configure' );
 		return $actions;
 	}
 
-	public function action_plugin_ui( $plugin_id, $action )
+	public function action_plugin_ui_configure()
 	{
-		if ( $plugin_id == $this->plugin_id() ) {
-			switch ( $action ) {
-				case _('Configure') :
-					$ui = new FormUI( strtolower( get_class( $this ) ) );
-					$user= $ui->append( 'text', 'user', 'lastrecent__user',  _t('Last.fm username: ' ) );
-					$limit= $ui->append( 'select', 'limit', 'lastrecent__limit', _t('Number of tracks to display: ') );
-					$options= array();
-					for ( $i= 1; $i<= 10; $i++ ) {
-						$options[$i]= $i;
-					}
-					$limit->options= $options;
-					$images= $ui->append( 'select', 'images', 'lastrecent__images', _t('Show images: ') );
-					$images->options= array( 'none' => 'none', 'small' => 'small', 'medium' => 'medium', 'large' => 'large' );
-					$ui->append( 'submit', 'save', _t('Save') );
-					$ui->out();
-					break;
-				}
-			}
+		$ui = new FormUI( strtolower( get_class( $this ) ) );
+		$ui->append( 'static', 'null:null', _t( 'These settings are only effective for themes using the legacy implementation method (See Help). If you plan to use blocks, you can must set these settings on a per-block basis.', 'lastrecent') );
+		$user= $ui->append( 'text', 'user', 'lastrecent__user',  _t( 'Last.fm username: ', 'lastrecent' ) );
+		$limit= $ui->append( 'select', 'limit', 'lastrecent__limit', _t( 'Number of tracks to display: ', 'lastrecent' ) );
+		$options= array();
+		for ( $i= 1; $i<= 10; $i++ ) {
+			$options[$i]= $i;
+		}
+		$limit->options= $options;
+		$images= $ui->append( 'select', 'images', 'lastrecent__images', _t( 'Show images: ', 'lastrecent' ) );
+		$images->options= array( 'none' => _t( 'None', 'lastrecent' ), 'small' => _t( 'Small', 'lastrecent' ), 'medium' => _t( 'Medium', 'lastrecent' ), 'large' => _t( 'Large', 'lastrecent' ), 'extralarge' => _t( 'Extralarge', 'lastrecent' ) );
+		$ui->append( 'submit', 'save', _t( 'Save' ) );
+		$ui->on_success( array( $this, 'saveOptions' ) );
+		$ui->out();
 	}
 
-	public function update_config( $ui )
+	// Save the form and clear the cache
+	public static function saveOptions ( $ui )
 	{
-		return true;
+		$ui->save();
+		Cache::expire( 'lastrecent__legacy' );
 	}
-
-	public function filter_lastrecent( $result )
+	
+	public function filter_block_list( $block_list )
 	{
-		$lastrecent= $this->get_lastrecent();
-		Cache::set( 'lastrecent', $lastrecent, $this->cache_expiry );
-		return $result;
+		$block_list['lastfm_recent'] = _t( 'Last.fm Recent Tracks', 'lastrecent' );
+		return $block_list;
 	}
-
+	
+	public function action_block_content_lastfm_recent( $block, $theme )
+	{
+		$block->lastfm_recent = $this->get_lastrecent( $block );
+	}
+	
+	public function action_block_form_lastfm_recent( $form, $block )
+	{
+		$form->append( 'text', 'user', $block,  _t( 'Last.fm username: ', 'lastrecent' ) );
+		$form->append( 'text', 'user', $block,  _t( 'Last.fm username: ', 'lastrecent' ) );
+		$form->append( 'select', 'limit', $block, _t( 'Number of tracks to display: ', 'lastrecent' ) );
+		$options = array();
+		for ( $i = 1; $i<= 10; $i++ ) {
+			$options[$i] = $i;
+		}
+		$form->limit->options = $options;
+		$form->append( 'select', 'size', $block, _t( 'Show images: ', 'lastrecent' ) );
+		$form->size->options= array( null => _t( 'None', 'lastrecent' ), 0 => _t( 'Small', 'lastrecent' ), 1 => _t( 'Medium', 'lastrecent' ), 2 => _t( 'Large', 'lastrecent' ), 3 => _t( 'Extralarge', 'lastrecent' ) );
+	}
+	
+	public function action_block_update_after( $block )
+	{
+		if ( $block->type == 'lastfm_recent' ) {
+			Cache::expire( 'lastrecent__' . $block->title );
+		}
+	}
+	
 	public function theme_lastrecent( $theme )
 	{
-		if ( Cache::has( 'lastrecent' ) ) {
-			$theme->lastfm_tracks= Cache::get( 'lastrecent' );
-		} else {
-			$theme->lastfm_tracks= $this->get_lastrecent();
-			Cache::set( 'lastrecent', $theme->lastfm_tracks, $this->cache_expiry );
-		}
+		$theme->lastfm_tracks= $this->get_lastrecent();
 		return $theme->fetch( 'lastfm' );
 	}
 
-	private function get_lastrecent()
+	private function get_lastrecent( $block = null )
 	{
-		$recent= '';
-		try {
-			$last= new RemoteRequest( 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key=' . $this->api_key . '&user=' . urlencode( $this->user ) . '&limit=' . $this->limit );
-			$last->set_timeout( 5 );
-			$result= $last->execute();
-			if ( Error::is_error( $result ) ) {
-				throw $result;
-			}
-			$response= $last->get_response_body();
-			$xml= new SimpleXMLElement( $response );
-			$recent= '<ul id="lastrecent">';
-			foreach( $xml->recenttracks->track as $track ) {
-				$recent.= '<li><a href="' . $track->url . '">';
-				if ( null !== $this->images ) {
-					$recent.= '<img src="' . $track->image[$this->images] . '" /><br>';
+		$name = ( $block ) ? $block->title : 'legacy';
+		$user = ( $block ) ? $block->user : $this->user;
+		$limit = ( $block ) ? $block->limit : $this->limit;
+		$size = ( $block ) ? (int) $block->size : $this->images;
+		
+		if ( Cache::has( 'lastrecent__' . $name ) ) {
+			$recent = Cache::get( 'lastrecent__' . $name );
+		} else {
+			try {
+				$last = new RemoteRequest( 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&api_key=' . $this->api_key . '&user=' . urlencode( $user ) . '&limit=' . $limit );
+				$last->set_timeout( 5 );
+				$result = $last->execute();
+				if ( Error::is_error( $result ) ) {
+					throw $result;
 				}
-				$recent.= $track->name . '</a> by ' . $track->artist . '</li>';
+				$response = $last->get_response_body();
+				$xml = new SimpleXMLElement( $response );
+				$recent = array();
+				foreach ( $xml->recenttracks->track as $track ) {
+					$recent[] = array( 'name' => (string) $track->name,
+									   'url' => (string) $track->url, 
+									   'image' => (string) $track->image[$size],
+									   'artist' => (string) $track->artist,
+									   'album' => (string) $track->album,
+									   'date' => (string) $track->date,
+								);
+				}
+				Cache::set( 'lastrecent__' . $name, $recent, $this->cache_expiry );
+			} catch( Exception $e ) {
+				$recent['error'] = $e->getMessage();
 			}
-			$recent.= '</ul>';
-		} catch( Exception $e ) {
-			$recent['error']= $e->getMessage();
 		}
 		return $recent;
 	}
-
-	public function action_update_check()
-	{
-		Update::add( 'RecentLast', $this->uuid, $this->info->version );
-	}
-
 }
 ?>
